647d74cead382b5063db93d987bbf0b05864cd8b
qhuu2742 <qhuu.official@gmail.com>
2022-12-12 12:51:15 +0700

    adding meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SummaryMeal from "./SummaryMeal";
+import AvailableMeal from "./AvailableMeal";
+
+const Meals = () => {
+  return (
+    <>
+      <SummaryMeal />
+      <AvailableMeal />
+    </>
+  );
+};
+
+export default Meals;
